45c9fa7e349b2c1bc40d22f5cff9533e47efd706
bowie <leticia.bini@gmail.com>
2026-03-27 15:20:13 -0300

    commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,7 +40,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0c5ae4237a79ddf72eed0b0bdbc53325
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "d20ce0afc1efddd2b5ff1dc90c008fcf",
+"flutter_bootstrap.js": "134481725a05ab365b29821c87878b69",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
